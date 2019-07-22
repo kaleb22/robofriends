eddc3c02068791c270dab3e5159929a337c39a6f
kaleb22 <kaleb22@hotmail.com>
2019-07-22 20:19:13 -0300

    deleting files that I don't need anymore and creating my first component, a card

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Billy Joe',
+        username: 'Billy',
+        email: 'billw@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Billy Graan',
+        username: 'Billy',
+        email: 'kkaka@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Joe007',
+        username: 'Jowwanah',
+        email: 'hioill@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Nathas',
+        username: 'Nath',
+        email: 'nath@gmail.com'
+    }
+]
